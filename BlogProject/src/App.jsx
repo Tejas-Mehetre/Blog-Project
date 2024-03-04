75a79bd1cdf412f3bff4c3e4c6e83afff24e2819
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-03-04 21:04:33 +0530

    Commit

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import About from "./Pages/About";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl text-yellow-500">App Component</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
